86f5c6abe6464f84554612537b0aa3d1458b53c9
Shubham Goel <shubh@AkashLappy.ios.local>
2020-05-20 17:14:45 +0530

    Fixed scroll issue and made minor UI changes

--- src/components/titles.js
+++ src/components/titles.js
@@ -5,7 +5,7 @@ const Titles = props => {
     <div>
       <h1 className="title-container__title">Weatheractor</h1>
       <h6 className="title-container__subtitle">
-        An elegant way to weather app
+        An elegant way to know your weather
       </h6>
     </div>
   );
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 const Weather = (props) => {
   return (
-    <div className="weather-info">
-      {
-        props.country && props.city &&<div className="location">{props.city}, {props.country}</div>
-      }
-      {
-        props.temperature && <div className="temp">{ Math.floor(props.temperature - 273.15) }°C</div>
-      }
-      {
-        props.humidity && <p className="weather__key">Humidity: <div className="weather__value">{props.humidity}</div></p>
-      }
-      {
-        props.description && <p className="weather__key">Conditions: <div className="weather__value">{props.description}</div></p>
-      }
-      {
-        props.error && <p className="weather__key"><div className="weather__value">{props.error}</div></p>
-      }
-    </div>
+    <div><div className="weather-info">
+    {
+    props.country && props.city &&<div className="location">{props.city}, {props.country}</div>
+    }
+    {
+    props.temperature && <div className="temp">{ Math.floor(props.temperature - 273.15) }°C</div>
+    }
+    {
+    props.humidity && <p className="weather__key">Humidity: <div className="weather__value">{props.humidity}</div></p>
+    }
+    {
+    props.description && <p className="weather__key">Conditions: <div className="weather__value capitalize">{props.description}</div></p>
+    }
+    {
+    props.error && <p className="weather__key"><div className="weather__value">{props.error}</div></p>
+    }
+    </div></div>
   )
 }
 
